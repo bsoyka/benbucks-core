5f19548c3fb3994bb8f5ecd1da033eb158e9d12f
Ben Soyka <hello@bsoyka.me>
2023-01-02 11:25:49 -0600

    Add money formatting utility

--- benbucks_core/_utils.py
+++ benbucks_core/_utils.py
@@ -1,6 +1,14 @@
-def _round_money(prize: int | float) -> int | float:
+CURRENCY_SYMBOL = "\u20bf"
+
+
+def _round_money(amount: int | float) -> int | float:
     """Round a currency amount to the nearest cent."""
-    rounded = round(prize, 2)
+    rounded = round(amount, 2)
 
     # If the rounded value is an integer, return it as an int
     return int(rounded) if rounded == int(rounded) else rounded
+
+
+def _format_money(amount: int | float) -> str:
+    """Format a currency amount as a string."""
+    return f"{CURRENCY_SYMBOL}{_round_money(amount):.2f}"
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from benbucks_core._utils import _round_money
+from benbucks_core._utils import CURRENCY_SYMBOL, _format_money, _round_money
 
 
 @pytest.mark.parametrize(
@@ -19,3 +19,17 @@ def test_round_currency(test_input: int | float, expected: int | float):
 def test_round_currency_type(test_input: int | float, expected: int | float):
     """Test result type of rounding."""
     assert type(_round_money(test_input)) == type(expected)
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    {
+        (1, "1.00"),
+        (1.23, "1.23"),
+        (1.2344444444, "1.23"),
+        (1.2355555555, "1.24"),
+    },
+)
+def test_format_money(test_input: int | float, expected: str):
+    """Test formatting of currency."""
+    assert _format_money(test_input) == CURRENCY_SYMBOL + expected
