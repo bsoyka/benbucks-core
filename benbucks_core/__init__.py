d0c642a93aaf9a8548de07387c75ad3832fb2bc8
Ben Soyka <hello@bsoyka.me>
2023-01-02 10:28:52 -0500

    Move db initialization to package

--- benbucks_core/__init__.py
+++ benbucks_core/__init__.py
@@ -1,5 +1,14 @@
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from .pool import Pool
 from .trivia import TriviaQuestion
 from .user import User
 
-__all__ = ["Pool", "TriviaQuestion", "User"]
+__all__ = ["init_db", "Pool", "TriviaQuestion", "User"]
+
+
+async def init_db(client: AsyncIOMotorClient, env: str = "prod"):
+    await init_beanie(
+        document_models=[Pool, TriviaQuestion, User], database=client[env]
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,10 @@
 import pytest
-from beanie import init_beanie
 from mongomock_motor import AsyncMongoMockClient
 
-from benbucks_core import Pool, TriviaQuestion, User
+from benbucks_core import init_db
 
 
 @pytest.fixture
 async def mongo_mock_client():
     client = AsyncMongoMockClient()
-    await init_beanie(
-        document_models=[Pool, TriviaQuestion, User],
-        database=client.get_database(name="db"),
-    )
+    await init_db(client, "test")
